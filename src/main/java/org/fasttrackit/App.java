804272f61c46fe5fc7066d412a62ea704e399d40
Florin Jurchis <jurchis@gmail.com>
2019-08-26 20:47:01 +0300

    Class 3 Part 1 raceGame

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,4 +1,7 @@
 package org.fasttrackit;
+//this is a one line comment
+//with ctrl q on the functions it helps you with the function
+//with green on the left side is showing what has been changed
 
 /**
  * Hello world!
@@ -6,8 +9,23 @@ package org.fasttrackit;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        //click in the current line and CTRL X will remove the line
+        //we create an object type Car, the class Car has already been created
+
+        Car car = new Car();
+
+        //Car() is the class constructor, it has been created in the memory, we don't have access to it so we'll create
+        //An object type Car Car car=new Car();
+        //We define the class Car and create a variable type car with it
+        //Classes are in uppercase while the vars in lowercase
+
+        car.name="Dacia";
+        car.fuelLevel = 60;
+        car.mileage = 8.5;
+        car.doorCount = 180;
+        car.fuelType = "Gasoline";
+        //All the time the code should be in EN
     }
 }
---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,18 @@
+package org.fasttrackit;
+
+//Clasele sunt tipuri de date complexe pe cand
+//Tipurile de date primitive sunt cu litere mici
+
+//am definit clasa Car ca si concept
+public class Car {
+
+    String name;
+    double mileage;
+    double maxSpeed;
+    String fuelType;
+    double fuelLevel;
+    double travelDistance;
+    int doorCount;
+    boolean running;
+
+}
